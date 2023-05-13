6c21c1fb76f00fe19b249e31332da589778e07eb
nikolaiser <nikolai.sergeev@unzer.com>
2023-05-13 12:06:20 +0200

    Disable tabnine

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -6,6 +6,7 @@ return {
       -- make sure mason installs the server
       servers = {
         rust_analyzer = {},
+        spectral_language_server = {},
       },
       setup = {
         rust_analyzer = function(_, opts)
@@ -208,6 +209,7 @@ return {
         "codelldb",
         "rust-analyzer",
         "taplo",
+        "spectral-language-server",
       },
     },
   },
